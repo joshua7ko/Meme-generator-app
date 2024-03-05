ce5fb26bb7d68ab90a97e6c67c2e878206b16bc1
Joshua <joshua7ko@gmail.com>
2024-03-05 16:59:46 +0530

    Added styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header(){
    return ( <header className="header">
               <img src="./public/troll-face.png" className="header-image"/>
               <h2 className="header-title">Meme Generator</h2>
-              <h4 className="header-project">React Course - Project 3</h4>
+              <h4 className="header-project">Project 3</h4>
             </header>
    )
 }
