f296444ad663b89080d28bf41cb332b948ec2277
Joshua <joshua7ko@gmail.com>
2024-03-15 20:50:20 +0530

    Added Import Images

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
+import {logoImg }  from "/public/troll-face.png";
 
 
 export default function Header(){
    return ( <header className="header">
-              <img src="./public/troll-face.png" className="header-image"/>
+              <img src={logoImg} className="header-image"/>
               <h2 className="header-title">Meme Generator</h2>
               <h4 className="header-project">Project 3</h4>
             </header>
